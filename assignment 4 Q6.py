f3640c345c8c281bf8fa26f50db2282a220a6706
harshvaish4071 <97724809+harshvaish4071@users.noreply.github.com>
2022-03-01 22:48:16 +0530

    Add files via upload

---
+++ assignment 4 Q6.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+import enchant
+d=enchant.Dict("en_US")
+oper=set ()
+user_inp= "freind"
+letter = [x.lower() for x in user_inp]
+for n in range(len(user_inp)):
+    for y in list (permutations (letter, len(user_inp))):
+        z="".join(y)
+        if d.check(z):
+            oper.add(z)
+print (oper)
